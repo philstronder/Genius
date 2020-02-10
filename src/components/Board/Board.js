a809ccbaf8f9ab37c2a6f2aa24a160ec79a3660b
Felipe Vale De Carvalho <felipevaledecarvalho@MacBook-Pro-de-Felipe.local>
2020-02-10 00:18:39 -0300

    first commit

---
+++ src/components/Board/Board.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { directive } from '@babel/types';
+import './Board.css'
+import $ from 'jquery';
+
+import { useEffect } from 'react';
+
+
+
+export default function Board() {
+    useEffect(() => {
+        alert('effect')
+        $('.container').click((e) => {
+            //Visually shows that the element is active
+            e.target.classList.add('active');
+            setTimeout(() => {
+                e.target.classList.remove('active');
+            }, 1000);
+
+            //Plays key audio
+            let audio;
+            if(e.target.classList.contains('first')) {
+                audio = new Audio('button1.mp3');
+                audio.play();
+            } else if (e.target.classList.contains('second')){
+                audio = new Audio('button2.mp3');
+                audio.play();
+            } else if (e.target.classList.contains('third')) {
+                audio = new Audio('button3.mp3');
+                audio.play();
+            } else if (e.target.classList.contains('fourth')) {
+                audio = new Audio('button4.mp3');
+                audio.play();
+            }
+            
+        });
+    });
+
+    return(
+        <div className='container'>
+            <div className='button first' id='btn1'></div>
+            <div className='button second' id='btn2'></div>
+            <div className='button third' id='btn3'></div>
+            <div className='button fourth' id='btn4'></div>
+        </div>
+    );
+}
